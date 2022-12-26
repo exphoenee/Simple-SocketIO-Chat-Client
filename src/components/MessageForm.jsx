ff38b92feec0a288abc49e8988c058ef6890e8c6
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-12-26 15:23:49 +0100

    Create MessageForm.jsx

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from "react";
+
+import { AllMessageContext } from "../contexts/allMessageContext";
+
+const MessageForm = () => {
+  const { setAllMessages, userId, socket } = useContext(AllMessageContext);
+  const [message, setMessage] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message === "") return;
+    console.log("Send message");
+    const data = { userId, message, date: Date.now() };
+    socket.emit("send_message", data);
+    setAllMessages((prev) => [...prev, data]);
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={sendMessage}>
+      <input
+        onChange={(e) => {
+          console.log(e.target.value);
+          setMessage(e.target.value);
+        }}
+        value={message}
+        placeholder="Message..."
+      />
+      <button type="submit">Send message</button>
+    </form>
+  );
+};
+
+export default MessageForm;
