223a5c11b1f7fd2a8bef84981be85648c53585fd
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-12-26 16:20:37 +0100

    Update MessagesBox.jsx

--- src/components/MessagesBox.jsx
+++ src/components/MessagesBox.jsx
@@ -1,18 +1,33 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext } from "react";
 
 import { AllMessageContext } from "../contexts/AllMessageContext";
 
 const MessagesBox = () => {
-  const { allMessages, socketId } = useContext(AllMessageContext);
+  const { allMessages, setAllMessages, userdata, socket } =
+    useContext(AllMessageContext);
+
+  useEffect(() => {
+    const handler = (data) => {
+      console.log(data);
+      setAllMessages((prev) => [...prev, data]);
+    };
+
+    socket.on("receive_message", handler);
+    return () => socket.off("receive_message", handler);
+  }, [socket]);
 
   return (
     <div>
       {allMessages.map((m, i) => (
         <p
-          style={{ textAlign: m.socketId === socketId ? "left" : "right" }}
+          style={{
+            textAlign: m.username === userdata?.username ? "left" : "right",
+          }}
           key={`${m.message}-${i}`}
         >
-          {m.socketId === socketId ? "me: " : `${m.socketId}: `}{" "}
+          {m.username === userdata?.username
+            ? "me: "
+            : `${userdata?.username}: `}
           {`${m.message}`}
         </p>
       ))}
