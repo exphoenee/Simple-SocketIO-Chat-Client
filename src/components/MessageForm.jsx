85a1daa34eb2851d95761ee17f0acf9dbf6e57be
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-12-26 15:38:32 +0100

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -3,14 +3,14 @@ import React, { useState, useContext } from "react";
 import { AllMessageContext } from "../contexts/allMessageContext";
 
 const MessageForm = () => {
-  const { setAllMessages, userId, socket } = useContext(AllMessageContext);
+  const { setAllMessages, socketId, socket } = useContext(AllMessageContext);
   const [message, setMessage] = useState("");
 
   const sendMessage = (e) => {
     e.preventDefault();
     if (message === "") return;
     console.log("Send message");
-    const data = { userId, message, date: Date.now() };
+    const data = { socketId, message, date: Date.now() };
     socket.emit("send_message", data);
     setAllMessages((prev) => [...prev, data]);
     setMessage("");
