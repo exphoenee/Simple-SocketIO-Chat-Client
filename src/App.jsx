ffc958e4f79678f16d9266ede965a07d6cae31d2
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-12-26 15:43:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import LoginForm from "./components/LoginForm";
 import MessageFrom from "./components/MessageForm";
 import MessagesBox from "./components/MessagesBox";
 
-import AllMessageContextProvider from "./contexts/allMessageContext";
+import AllMessageContextProvider from "./contexts/AllMessageContext";
 
 import checkUserStorage from "./helpers/checkUserStorage";
 
@@ -15,9 +15,9 @@ const socket = io.connect("http://localhost:3000");
 
 function App() {
   const [allMessages, setAllMessages] = useState([]);
-  const user = checkUserStorage();
 
   const socketId = socket.id;
+  const user = checkUserStorage(socket);
 
   console.log(socket);
 
@@ -34,14 +34,13 @@ function App() {
   return (
     <div className="App">
       <AllMessageContextProvider
-        value={{ allMessages, setAllMessages, socketId, socket }}
+        value={{ allMessages, setAllMessages, socket }}
       >
         <header className="App-header">
           <h1>Socket.io chat app</h1>
         </header>
         <body>
-          <MessageFrom />
-          {user ? <MessagesBox /> : <LoginForm />}
+          {user ? <MessageFrom /> : <LoginForm />} <MessagesBox />
         </body>
       </AllMessageContextProvider>
     </div>
